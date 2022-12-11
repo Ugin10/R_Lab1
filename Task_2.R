b3c34e9b3578b2269c3246e789f3903bc9618203
Ugin10 <72548462+Ugin10@users.noreply.github.com>
2022-12-11 16:43:44 +0300

    Delete Task_2.R

--- Task_2.R
+++
@@ -1,8 +0,0 @@
-get_id <- function(dfs) {
-  new_df <- Reduce(function(x, y) merge(x, y, by = "id"), dfs)
-  result <- data.frame(id=new_df[, 1], mean=rowMeans(new_df[, -1]))
-  return (result[complete.cases(result), ])
-}
-
-load("/Documents/POLYTECH/7_SEM/R/Lab_1/lab1_e2.Rdata")
-get_id(all_data)
