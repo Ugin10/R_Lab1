9097a9e586895d63a06af658507baf3f3e37a189
Ugin10 <72548462+Ugin10@users.noreply.github.com>
2022-12-11 16:44:13 +0300

    Add files via upload

---
+++ First_Task.R
@@ -0,0 +1,32 @@
+# гюдюмхе 1
+
+setwd("D:\\Documents\\POLYTECH\\7_SEM\\R")
+
+df <- read.csv(file = "Lab_1\\lab1_e1.csv")
+
+change_to_number <- function(column){
+  if (length(grep('[a-z]', column, ignore.case = T)) == 0){
+    column <- as.double(gsub(" ", "", column))
+  }
+  ret?rn(column)
+}
+
+fix_data <- function(df) {
+  data.frame(lapply(df, change_to_number))
+}
+
+task1_result <- fix_data(df)
+print(task1_result)
+
+
+# гюдюмхе 2
+get_id <- function(dfs) {
+  new_df <- Reduce(function(x, y) merge(x, y, by = "id"), dfs)
+  result <- data.?rame(id=new_df[, 1], mean=rowMeans(new_df[, -1]))
+  return (result[complete.cases(result), ])
+}
+
+load("/Documents/POLYTECH/7_SEM/R/Lab_1/lab1_e2.Rdata")
+result_t <- get_id(all_data)
+print(result_t)
+
